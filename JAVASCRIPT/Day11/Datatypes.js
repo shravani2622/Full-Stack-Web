0798377c99267d9713e0c3583571f0f5d19722f3
Shravani Devarshetti <shravani2622>
2024-09-23 11:39:58 +0530

    Day11

---
+++ JAVASCRIPT/Day11/Datatypes.js
@@ -0,0 +1,49 @@
+var greet = "Hello,  Good Evening";
+//var message ="Welcome to \t javascript live classes";
+//var message ="Welcome to \n javascript live classes";
+//var message ="Welcome to \r javascript live classes";
+//var message ="Welcome to \v javascript live classes";
+
+
+
+//console.log(greet +" "+ message)
+//console.log(message)
+//console.log(greet.length)
+//console.log(greet.charAt(8))
+//console.log(greet.toUpperCase())
+//console.log(greet.toLowerCase())
+ //var num = "123";
+// var num1 = parseInt(num)
+ //var num = "123.4589";
+ //var num1 = parseFloat(num)
+ //var num1 = parseInt(num)
+
+ //console.log(num)
+ //console.log(num1.toFixed(3))
+ //console.log("1" + "1")
+ //console.log("1" * "A")
+ //console.log("1" + "A")
+ //console.log("1"+1)
+ //console.log(1+1)
+ //console.log(1-1)
+ //console.log("1" - "1")
+ //console.log(Math.sqrt(625))
+ //console.log(Math.abs(-10))
+ //console.log(Math.max(1,2,34,5,6))
+ //console.log(Math.min(1,2,3,4,56))
+
+ //var ismarried = false;
+ //console.log(ismarried)
+ //var age = 18;
+ //console.log(age==18)
+ //var num = Boolean(1)
+ //var num = Boolean()
+ //var num = Boolean("")
+ //var num = Boolean(-1)
+ //var num = Boolean(0.09)
+// console.log(num)
+//var num = null;
+//console.log(num)
+var num = 
+console.log(num)
+
---
+++ JAVASCRIPT/Day11/Datatypestask.js
@@ -0,0 +1,27 @@
+//1.String Task:
+  var favoriteFood = "Panner Biriyani";
+  console.log(favoriteFood)
+//2.Number Task:
+  var myAge = 21;
+  console.log(myAge)
+//3.Boolean Task:
+ var isStudent = "student";
+ console.log(isStudent== "student")
+//4.Undefined Task:
+  var myPet
+  console.log(myPet)
+ //5.Null Task:
+  var carBrand = null;
+  console.log(carBrand)
+
+ console.log("1"+"4"+"1")       =141
+ console.log("A"-"B")           =NaN
+ console.log("A"-"B"+"1")       =NaN1
+ console.log("A"-"B"+1)         =Nan
+ console.log("1"-"1")           =0
+ console.log(+"1"+"1"+"1")      =111
+ console.log(1 + + "1" + "1")   =21
+ console.log(1 - "1" + "1")     =01
+ console.log(+ "")              =0
+
+ 
